ec16debddee14cb8b2a61dd1d3ef7be80947be9f
Trần Ngọc Đức <caothu91@gmail.com>
2018-06-20 08:06:42 +0700

    hien thi chi tiet

--- application/views/salary.php
+++ application/views/salary.php
@@ -1,39 +1,35 @@
 <!-- Luong -->
 <div class="container-fluid">
 
-	<!-- Nav tabs -->
-	<ul class="nav nav-tabs" role="tablist">
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 		<?php foreach ($salary as $name => $ns): ?>
-			<li role="presentation">
-				<a href="#<?php echo implode('-', explode(' ', strtolower($name))); ?>"
-				   role="tab"
-				   aria-controls="<?php echo implode('-', explode(' ', strtolower($name))); ?>"
-				   data-toggle="tab">
-					<?php echo $name; ?>
-				</a>
-			</li>
-		<?php endforeach; ?>
-
-		<li role="presentation">
-			<a href="#debug" aria-controls="debug" role="tab" data-toggle="tab">Debug</a>
-		</li>
-	</ul>
-
-	<!-- Tab panes -->
-	<div class="tab-content">
-		<?php foreach ($salary as $name => $ns): ?>
-			<div role="tabpanel" class="tab-pane"
-				 id="<?php echo implode('-', explode(' ', strtolower($name))); ?>">
-				<div class="nhansu">
-					<?php $this->load->view('salary/nhansu', array('nhansu' => $ns)); ?>
+			<div class="panel panel-default">
+				<div class="panel-heading"
+					 role="tab"
+					 id="heading<?php echo implode('-', explode(' ', strtolower($name))); ?>">
+					<h4 class="panel-title">
+						<a role="button"
+						   data-toggle="collapse"
+						   data-parent="#accordion"
+						   href="#<?php echo implode('-', explode(' ', strtolower($name))); ?>"
+						   aria-expanded="true"
+						   aria-controls="<?php echo implode('-', explode(' ', strtolower($name))); ?>">
+							<?php echo $name; ?>
+						</a>
+					</h4>
+				</div>
+				<div id="<?php echo implode('-', explode(' ', strtolower($name))); ?>"
+					 class="panel-collapse collapse"
+					 role="tabpanel"
+					 aria-labelledby="<?php echo implode('-', explode(' ', strtolower($name))); ?>">
+					<div class="panel-body">
+						<div class="nhansu">
+							<?php $this->load->view('salary/nhansu', array('nhansu' => $ns)); ?>
+						</div>
+					</div>
 				</div>
 			</div>
 		<?php endforeach; ?>
-		<div role="tabpanel" class="tab-pane" id="debug">
-			<pre>
-				<?php print_r($salary); ?>
-			</pre>
-		</div>
 	</div>
 
 </div>
--- application/views/salary/chitiet.php
+++ application/views/salary/chitiet.php
@@ -1,22 +1,26 @@
 <table class="table table-hover">
 	<tr>
-		<th></th>
-		<th></th>
-		<th></th>
-		<th></th>
-		<th></th>
+		<th>#</th>
+		<th>Xe</th>
+		<th>Năng suất xe</th>
+		<th>Tỉ lệ chia</th>
+		<th>Cho nợ</th>
+		<th>Thu nợ</th>
+		<th>Năng suất</th>
+		<th>Hệ số</th>
+		<th>Lương</th>
 	</tr>
 	<?php foreach ($nhansu->getTuyen() as $date => $ns): ?>
 		<tr>
-			<td><?php echo date('d/m/Y', ($date - 25569) * 86400); ?></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
+			<th><?php echo date('d/m/Y', ($date - 25569) * 86400); ?></th>
+			<td><?php echo $ns['xe']; ?></td>
+			<td><?php echo $ns['nang suat xe']; ?></td>
+			<td><?php echo number_format((float)$ns['ti le'], 2, '.', ''); ?></td>
+			<td><?php echo $ns['cho no']; ?></td>
+			<td><?php echo $ns['thu no']; ?></td>
+			<td><?php echo number_format((float)$ns['nang suat'], 2, '.', ''); ?></td>
+			<td><?php echo $ns['ti suat']; ?></td>
+			<td><?php echo number_format((float)$ns['luong'], 2, '.', ''); ?></td>
 		</tr>
 	<?php endforeach; ?>
 </table>
-
-<pre>
-	<?php print_r($nhansu); ?>
-</pre>
